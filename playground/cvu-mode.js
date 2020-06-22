2a111f4187d0072b24c32059efc88fbf1c106bdf
mkslanc <mks-tech@ya.ru>
2020-06-23 01:34:07 +0300

    new keywords and states for cvu mode highlight
    # Conflicts:
    #	playground/cvu-mode.js

--- playground/cvu-mode.js
+++ playground/cvu-mode.js
@@ -7,56 +7,120 @@ var oop = ace.require("ace/lib/oop");
 var TextMode = ace.require("ace/mode/text").Mode;
 var FoldMode = ace.require("ace/mode/folding/cstyle").FoldMode;
 var TextHighlightRules = ace.require("ace/mode/text_highlight_rules").TextHighlightRules;
-var CstyleBehaviour = ace.require("ace/mode/behaviour/cstyle").CstyleBehaviour;
 
 var CvuHighlightRules = function() {
     var keywordMapper = this.createKeywordMapper({
         "constant.language":
             "false|Infinity|NaN|nil|no|null|null|off|on|super|this|true|undefined|yes",
+        "support.type":
+            "left|top|right|bottom|center|lefttop|topleft|topright|righttop|leftbottom|bottomleft|rightbottom|bottomright",
+        "support.constant":
+            "regular|bold|semibold|heavy|light|ultralight|black",
         "keyword":
-            "and|AND|or|OR|equals|EQUALS|not|NOT"
+            "and|AND|or|OR|equals|EQUALS|not|NOT|"+
+            "back|addDataItem|openView|openViewByName|toggleEditMode|toggleFilterPanel|star|showStarred|showContextPane|showNavigation|share|duplicate|schedule|addToList|delete|setRenderer|select|selectAll|unselectAll|openLabelView|showSessionSwitcher|forward|forwardToFront|backAsSession|openSession|openSessionByName|addSelectionToList|closePopup|noop"
     }, "identifier");
+
+    var parensMap = {
+        "}": "{",
+        "]": "["
+    };
 
     // regexp must not have capturing parentheses. Use (?:) instead.
     // regexps are ordered -> the first match is used
     this.$rules = {
         "start": [
+            {include: "commentOpen"},
+            {include: "selectorBlockOpen"},
             {
-                token: "comment.start",
-                regex: "\\/\\*",
-                next: "comment"
+                token: "paren.lparen",
+                regex: /[[{]/,
+                next: "innerBlock",
+                onMatch: function (value, currentState, stack) {
+                    stack.unshift(this.next, value, currentState);
+                    return this.token;
+                },
             }, {
                 token: "paren.lparen",
-                regex: "[[({]"
+                regex: /[(]/
             }, {
                 token: "paren.rparen",
-                regex: "[\\])}]"
+                regex: /[)]/
             }, {
                 token: "text",
-                regex: "\\s+"
+                regex: /\s+/
+            },
+        ],
+        "innerBlock": [
+            {include: "commentOpen"},
+            {include: "selectorBlockOpen"},
+            {
+                token: "paren.lparen",
+                regex: /[[{]/,
+                next: "innerBlock",
+                onMatch: function (value, currentState, stack) {
+                    stack.unshift(this.next, value, currentState);
+                    return this.token;
+                },
+            }, {
+                token: "paren.rparen",
+                regex: /[}\]]/,
+                next: "start",
+                onMatch: function (value, currentState, stack) {
+                    if (parensMap[value] == stack[1]) {
+                        stack.shift();
+                        stack.shift();
+                        this.next = stack.shift();
+                    } else {
+                        this.next = '';
+                    }
+                    return this.token;
+                }
             }, {
                 token: "variable",
-                regex: "\\b\\w+(?=\\s*:)"
+                regex: /\b\w+(?=\s*:)/
             },
             {
                 include: "expressions"
             }
         ],
-        "selectorProp": [{
-            token: "",
-            regex: /(?=\{)|$/,
-            next: "pop",
-        }],
-        "selector": [{
-            
-        }],
+        "selectorBlockOpen": [
+            {
+                token: ["paren.lparen", "keyword"],
+                regex: /(\[)(?:\s*)(sessions|session|renderer|datasource|color|style|language)/,
+                push: "selectorBlock"
+            },
+        ],
+        "selectorBlock": [
+            {
+                token: "paren.rparen",
+                regex: /]/,
+                next: "pop"
+            }, {
+                token: "keyword.operator",
+                regex: /[=]/
+            }, {
+                include: "stringOpen"
+            },
+        ],
+        "stringOpen": [
+            {
+                token: "string",
+                regex: /["']/,
+                next: "string",
+                onMatch: function (value, currentState, stack) {
+                    stack.unshift(this.next, value, currentState);
+                    return this.token;
+                },
+            }
+        ],
         "string": [
             {
                 token: "constant.language.escape",
-                regex: /\\./
+                regex: /\\(?:x[0-9a-fA-F]{2}|u[0-9a-fA-F]{4}|['"\\\/bfnrt])/
             }, {
                 token: "string",
-                regex: '{',
+                regex: /{/,
                 push: "expressions",
             }, {
                 token: "string",
@@ -76,55 +140,56 @@ var CvuHighlightRules = function() {
                 defaultToken: "string"
             }
         ],
+        "commentOpen": [
+            {
+                token: "comment.start",
+                regex: /\/\*/,
+                push: "comment"
+            },
+        ],
         "comment": [
             {
                 token: "comment.end",
-                regex: "\\*\\/",
-                next: "start"
+                regex: /\*\//,
+                next: "pop"
             }, {
                 defaultToken: "comment"
             }
         ],
         "expressions": [
             {
-                token: "string",
-                regex: '["\']',
-                next: "string",
-                onMatch: function (value, currentState, stack) {
-                    stack.unshift(this.next, value, currentState);
-                    return this.token;
-                },
+                include: "stringOpen"
             },
             {
                 token: "constant.numeric", // hex
-                regex: "(?:0[xX]|#)[0-9a-fA-F]+\\b"
+                regex: /(?:0[xX]|#)[0-9a-fA-F]+\b/
             }, {
                 token: "constant.numeric", // float
-                regex: "[+-]?\\d+(?:(?:\\.\\d*)?(?:[eE][+-]?\\d+)?)?\\b"
+                regex: /[+-]?\d+(?:(?:\.\d*)?(?:[eE][+-]?\d+)?)?\b/
             }, {
                 token: "constant.language.boolean",
-                regex: "(?:[Tt]rue|[Ff]alse)\\b"
+                regex: /(?:[Tt]rue|[Ff]alse)\b/
             },
             {
                 token: keywordMapper,
-                regex: "\\b\\w+\\b"
+                regex: /\b\w+\b/
             }, {
                 token: "keyword.operator",
-                regex: "[=!+*/-]"
+                regex: /[=!+*/-]/
             }, {
                 token: "punctuation.operator",
-                regex: "[?:.,;]"
+                regex: /[?:.,;]/
             },
-            {
+            /*{
                 token: "paren.lparen",
-                regex: "[[(]"
+                regex: /[[(]/
             }, {
                 token: "paren.rparen",
-                regex: "[\\])]"
-            },
+                regex: /[\])]/
+            },*/
             {
                 token: "string",
-                regex: "}",
+                regex: /}/,
                 onMatch: function (value, currentState, stack) {
                     if (stack[0] == "expressions") {
                         stack.shift();
@@ -145,7 +210,6 @@ exports.CvuHighlightRules = CvuHighlightRules;
 
 var Mode = function() {
     this.HighlightRules = CvuHighlightRules;
-    this.$behaviour = new CstyleBehaviour();
     this.foldingRules = new FoldMode();
 };
 oop.inherits(Mode, TextMode);
