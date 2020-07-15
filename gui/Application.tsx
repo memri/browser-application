efd6945cc1b2ba919903819691f5929aa27ab896
mkslanc <mks-tech@ya.ru>
2020-07-15 21:10:22 +0300

    started convertation of gui files

---
+++ gui/Application.tsx
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  memri
+//
+//  Copyright © 2020 memri. All rights reserved.
+//
+
+import React from "react";
+import {Color} from "../parsers/cvu-parser/CVUParser";
+import {ScreenSizer} from "../extension/SwiftUI/ScreenSize";
+import {NavigationWrapper} from "./navigation/NavigationView";
+
+/*class View {
+	fullHeight(): View {
+		frame(0,
+			.infinity,
+			0, maxHeight: .infinity,
+			Alignment.topLeading)
+	}
+
+	fullWidth():View {
+		frame(0, .infinity, Alignment.topLeading)
+	}
+}*/
+
+/*var memri_shouldUseLargeScreenLayout: Bool {
+	#if targetEnvironment(macCatalyst)
+		return true
+	#else
+		return UIDevice.current.userInterfaceIdiom == .pad
+	#endif
+}*/
+
+class Application extends React.Component {
+	context: MemriContext
+	render() {
+		return (
+			<ScreenSizer background={new Color("systemBackground").toLowerCase()} colorScheme="light">
+				<VStack spacing={0}>
+					<NavigationWrapper isVisible={this.context.showNavigationBinding}>
+						{(this.context.showSessionSwitcher) ? <SessionSwitcher/>: <Browser/>}
+					</NavigationWrapper>
+					<DebugConsole/>
+				</VStack>
+			</ScreenSizer>
+			);
+	}
+}
+
+/*struct Application_Previews: PreviewProvider {
+	static var previews: some View {
+		let context = try! RootContext(name: "", key: "").mockBoot()
+		return Application().environmentObject(context)
+	}
+}*/
