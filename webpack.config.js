c1048b129a8b5ba2ed30b33dae6f0dde73a1654f
mkslanc <mks-tech@ya.ru>
2020-12-10 00:10:54 +0300

    added different config to split developers version from production

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,59 @@
 "use strict";
-module.exports = {
-    devtool: 'source-map',
-    entry: {
-        simple: './demo.ts',
-        react: './demo-react.tsx',
-    },
-    module: {
-        rules: [{
-            test: /\.(t|j)sx?$/,
-            use: {
-                loader: 'ftst-loader',
-                options: {
-                    transpileOnly: true,
-                    transformNullishCoalesce: true
-                }
-            },
-            exclude: /node_modules/,
-        }],
-    },
-    resolveLoader: {
-        modules: [
-            "node_modules", 
-            __dirname + "/node_modules",
-        ],
-    },
-    resolve: {
-        extensions: [ '.tsx', '.ts', '.js' ],
-    },
-    output: {
-        filename: 'bundle.[name].js',
-        path: __dirname + '/dist',
-    },
-    devServer: {
-        contentBase: __dirname,
-        compress: true,
-        port: 9000
-    },
+module.exports = (env, argv) => {
+    let module;
+    if (argv['ftst']) {
+        module = {
+            rules: [{
+                test: /\.(t|j)sx?$/,
+                use: {
+                    loader: 'ftst-loader',
+                    options: {
+                        transpileOnly: true,
+                        transformNullishCoalesce: true,
+                        configFile: 'ftst-config.json'
+                    }
+                },
+                exclude: /node_modules/,
+            }],
+        }
+    } else {
+        module = {
+            rules: [{
+                test: /\.(t|j)sx?$/,
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true
+                    }
+                },
+                exclude: /node_modules/,
+            }],
+        }
+    }
+    return {
+        devtool: 'source-map',
+        entry: {
+            simple: './demo.ts',
+            react: './demo-react.tsx',
+        },
+        module: module,
+        resolveLoader: {
+            modules: [
+                "node_modules",
+                __dirname + "/node_modules",
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        output: {
+            filename: 'bundle.[name].js',
+            path: __dirname + '/dist',
+        },
+        devServer: {
+            contentBase: __dirname,
+            compress: true,
+            port: 9000
+        },
+    }
 };
